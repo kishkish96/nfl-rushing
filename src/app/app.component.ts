0cef357e2694c72e2119bc78dc776a0eec55772a
kishkish96 <41966599+kishkish96@users.noreply.github.com>
2022-03-25 12:53:04 -0400

    Added table with json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { MatSort } from '@angular/material/sort';
+import { NflPlayerStatsService } from './services/nfl-player-stats.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  
+  nflPlayerRushing: any = [];
+  displayedColumns: string[] = [
+    'Player', 
+    'Team', 
+    'Pos', 
+    'Att', 
+    'Att/G',
+    'Yds',
+    'Avg',
+    'Yds/G',
+    'TD',
+    'Lng',
+    '1st',
+    '1st%',
+    '20+',
+    '40+',
+    'FUM'
+  ];
+
+  @ViewChild(MatSort) sort: MatSort;
+
+  constructor(
+    private nflPlayerStatsService: NflPlayerStatsService
+  ) {
+    this.nflPlayerStatsService.getNFLPlayerStats().subscribe((results) => {
+      this.nflPlayerRushing = results;
+      console.log(this.nflPlayerRushing);
+    });
+  }
 }
